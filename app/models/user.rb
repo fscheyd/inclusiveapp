b6ddeef154b727ff9f815910f239367866aafb1d
Freedom Scheyd <joefreedomsch@gmail.com>
2021-02-28 14:10:37 -0500

    fixed user model typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :reveiws
-  has_many :businesses, through :reviews
+  has_many :businesses, through: :reviews
   has_secure_password
 
   validates :username, uniqueness: true
