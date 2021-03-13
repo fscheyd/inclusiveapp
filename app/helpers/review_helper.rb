a7f2b0e1373dabae5f62d8a84669342bdadc5bee
Freedom Scheyd <joefreedomsch@gmail.com>
2021-03-12 22:26:57 -0500

    fixed review helpers

--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,2 +1,16 @@
 module ReviewHelper
+    def review_header(f)
+        out = ''
+        if @business
+            out << 'Business: '
+            out << @business.business_name + '<br>'
+        else
+            out << (f.label 'Business: ')
+            out << (f.select :business_id, @businesses)  + '<br>'
+        end
+
+        out.html_safe
+
+    
+    end
 end
