4e410572a6f047cb880d7036cd081802cc59feb1
Freedom Scheyd <joefreedomsch@gmail.com>
2021-06-01 23:09:20 -0400

    routes v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,19 @@
 Rails.application.routes.draw do
-  
-  root 'sessions#home'
-
+  root :to => "sessions#home"
+  get 'sessions/home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get '/users/:id', to: 'users#show'
-
+  get '/users/:id', to: 'users#show', as: "user"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-
-  match '/auth/:provider/callback', to: 'sessions#omniauth', via: [:get, :post]
-  
+  delete '/logout', to: 'sessions#destroy'
   resources :businesses do
     resources :reviews, only: [:index, :new, :create]
   end
-
-  resources :users 
-
-  resources :reviews
-
+  resources :users do
+    resources :reviews, only: [:index, :new, :create, :edit, :destroy]
+  end
 end
-  
+
   
 
