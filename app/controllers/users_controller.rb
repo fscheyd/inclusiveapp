5b41fe316a9768613ce02e25ec37ebfa685702a7
Freedom Scheyd <joefreedomsch@gmail.com>
2021-06-01 23:07:05 -0400

    Refoctor users controller, with help of DJ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,29 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :omniauth, :create]
-
+  
   def new
     @user = User.new
   end
-    
+  
   def create
     @user = User.new(user_params)
-      #byebug
       if @user.save
-        session[:user_id] = @user_id
-        redirect_to user_path(@user)
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
       else
-        render "/sessions/home"
+        render :new
       end
-      
   end
-
-  def show
-    @user = User.find_by_id(params[:id])
-    redirect_to '/' if !@user
-  end
-      
-  def omniauth
-    user = User.create_from_omniauth(auth)
-    if user.valid?
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-    else
-      flash[:message] = "Invalid Entry, please try again!"
-      redirect_to login_path
-    end
-end
   
-
+  def show
+    redirect_if_not_logged_in
+    @user = current_user
+    @reviews = current_user.reviews
+  end
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
-  end
-  
-  def require_user_access
-    user = User.find_by(id: params[:id])
-    unless current_user == user
-      flash[:error] = "Sorry, no can do."
-      redirect_to user_path(user)
-    end
-  end
-  
-  def auth
-    request.env['omniauth.auth']
   end
 
 end
